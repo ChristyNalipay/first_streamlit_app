fe584402b5aed73fc575309f2bbadc746dbc8397
ChristyNalipay <136980514+ChristyNalipay@users.noreply.github.com>
2023-06-19 19:31:06 +0800

    Update streamlit_app.py
    Added a button to load the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,9 @@ def get_fruit_load_list():
     my_cur.execute ("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall() 
+# Add a button to load the list
+if streamlit.button('Get Fruit List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   my_cnx.close()
+   streamlit.dataframe(my_data_rows)
