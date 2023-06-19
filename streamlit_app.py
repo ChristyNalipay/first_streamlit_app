e3ba8e9b4ea15998e01aa4e7c0478e0184cbb254
ChristyNalipay <136980514+ChristyNalipay@users.noreply.github.com>
2023-06-19 19:29:30 +0800

    Update streamlit_app.py
    added stremlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,18 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
 
+#Query Some Data
+#my_cnx = snowflake.connector.connect(**streamlit . secrets["snowflake"])
+#my_cur = my_cnx. cursor()
+#my_cur . execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.text ("The fruit load list contains:")
+streamlit.text ("View Our Fruit List - Add Your Favorites!")
+#snowflake related functions
