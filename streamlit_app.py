fce34cef6c7307543d8ca891c1d012aee0840e4a
ChristyNalipay <136980514+ChristyNalipay@users.noreply.github.com>
2023-06-19 20:44:27 +0800

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,15 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
-streamlit.text ("View Our Fruit List - Add Your Favorites!")
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute ("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
-    my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall() 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])#my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()#streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
