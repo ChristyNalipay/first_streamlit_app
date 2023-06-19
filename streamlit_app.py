e19fa153686fce88fe4256f298d488f162f280c1
ChristyNalipay <136980514+ChristyNalipay@users.noreply.github.com>
2023-06-19 20:34:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,10 @@ import snowflake.connector
 #my_data_row = my_cur.fetchone()#streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 streamlit.text ("View Our Fruit List - Add Your Favorites!")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute ("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall() 
 
 
