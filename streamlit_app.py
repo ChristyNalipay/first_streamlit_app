6264eecd3fd9f94e873b3ce16f61a6b0a322aeb7
ChristyNalipay <136980514+ChristyNalipay@users.noreply.github.com>
2023-06-20 10:05:02 +0800

    Update streamlit_app.py
    change typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,13 +73,14 @@ streamlit.text ("View Our Fruit List - Add Your Favorites!")
 #snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
+    my_cur.execute ("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
     my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall() 
-    
+    return my_cur.fetchall()  
 # Add a button to load the list
 if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
   
 #Allow the end user to add a fruit to the list
